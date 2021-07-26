77868301901a68d7105c823f00453a8b7698700b
codervinicius <vinicaixx@gmail.com>
2021-07-26 20:17:07 -0300

    att

--- src/routerComponents/auth/Signup.js
+++ src/routerComponents/auth/Signup.js
@@ -166,16 +166,23 @@ function Signup() {
         name="phoneNumber"
         required={false}
       />
-      <TextInput
-        label="Role"
-        id="role"
-        type="text"
-        value={state.role}
-        onChange={handleChange}
-        name="role"
-        required
-      />
-
+       <div>
+      <label htmlFor="role">Role</label>
+      <div className="input-group mb-3">
+        <select
+         label="Role"
+         id="role"
+         value={state.role}
+         onChange={handleChange}
+         name="role"
+         required
+          className="form-control"
+          >
+            <option value="ADMIN">Admin</option>
+            <option value="USER" selected>User</option>
+          </select>
+      </div>
+    </div>
       <div className="form-group">
         <button className="btn btn-primary" type="submit">
           Save
