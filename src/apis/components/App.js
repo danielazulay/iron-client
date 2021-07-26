17dcaafa67261edf35a99d2371192e0d6149532a
GianLucas <gianspf@gmail.com>
2021-07-26 20:44:42 +0200

    GianLucas - FirstCommit Client

---
+++ src/apis/components/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AuthContextComponent } from "../../contexts/authContext";
+// import ProtectedRoute from "../../routerComponents/auth/PrivateRouter";
+
+
+import Signup from "../../routerComponents/auth/Signup";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <AuthContextComponent>
+
+
+          <div className="container mt-5">
+            <Route path="/signup" component={Signup} />
+          </div>
+
+
+        </AuthContextComponent>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
