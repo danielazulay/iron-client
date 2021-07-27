c1f1065e65a729f229cf40e6a8951811831e2b01
codervinicius <vinicaixx@gmail.com>
2021-07-27 11:01:44 -0300

    mudança de pasta

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { AuthContextComponent } from "../../contexts/authContext";
+import { AuthContextComponent } from "../contexts/authContext";
 // import ProtectedRoute from "../../routerComponents/auth/PrivateRouter";
 
 
-import Signup from "../../routerComponents/auth/Signup";
+import Signup from "../routerComponents/auth/Signup";
 
 function App() {
   return (
