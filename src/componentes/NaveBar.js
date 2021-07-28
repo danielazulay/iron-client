2ca711d468daee4df2f1ec5358ac582ab1133dcc
Daniel Azulay <you@example.comdanielazulay20@gmail.com>
2021-07-28 10:47:02 -0300

    cart

--- src/componentes/NaveBar.js
+++ src/componentes/NaveBar.js
@@ -6,7 +6,7 @@ import Nav from 'react-bootstrap/Nav'
 
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
-
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 function NavBar() {
 
@@ -16,15 +16,22 @@ function NavBar() {
 <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
   <Navbar.Brand href="/">Home</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="/signup">Sign Up</Nav.Link>
-      <Nav.Link href="/Login">Login</Nav.Link>
-    
-    </Nav>
+  <div className="d-flex justify-content-end">
+    <Navbar.Brand href="/"  >  <i class="fas fa-shopping-cart"></i></Navbar.Brand>
+  
+<div>
+    <NavDropdown title="Menu" id="navbarScrollingDropdown">
+        <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+        <NavDropdown.Item href="/signup">Sign Up</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+      </NavDropdown>
+</div>
 
-  </Navbar.Collapse>
+</div>
+
+
+
   </Container>
 </Navbar>
 
